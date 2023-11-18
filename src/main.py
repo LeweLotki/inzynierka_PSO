70e49fe5c07b8a6682e36fdddd748b96e76d1a45
astefanczyk <astefanczyk@student.agh.edu.pl>
2023-11-18 18:59:35 +0100

    creating main file

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import argparse
+
+def default_message():
+    
+    print("\r No option specified. \n\r Type '--help' for list of arguments.")
+
+if __name__ == "__main__":
+    
+    description_path = './attributes/description.txt'
+    with open(description_path, 'r') as file:
+        program_description = file.read().strip()
+    
+    parser = argparse.ArgumentParser(description=program_description)
+
+    # Define command-line options
+    parser.add_argument('-a', action='store_true', help='data analysis')
+    parser.add_argument('-e', action='store_true', help='data extraction')
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Check which option was provided and execute the corresponding function
+    if args.a: pass
+    elif args.e: pass
+    else: default_message()
