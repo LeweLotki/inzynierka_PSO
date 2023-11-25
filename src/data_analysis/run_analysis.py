c8bfbb326063b70d69683b4783437b77d171525f
astefanczyk <astefanczyk@student.agh.edu.pl>
2023-11-25 01:00:41 +0100

    improving main function arguments

--- src/data_analysis/run_analysis.py
+++ src/data_analysis/run_analysis.py
@@ -1,12 +1,21 @@
-import subprocess
+from subprocess import (
+    run, CalledProcessError
+)
 
-notebook_file_path = './data_analysis/notebook.ipynb'
+from os import path
 
-def run_analysis(file_path=notebook_file_path):
+def run_analysis(notebook_file_name: str):
+    
+    notebook_folder_path = './data_analysis/notebooks/'
+    notebook_file_path = notebook_folder_path + notebook_file_name
+    
     try:
+        if not path.exists(notebook_file_path):
+            print(f"Error: Notebook file '{notebook_file_path}' not found.")
+            return
         # Use the subprocess module to run the jupyter notebook command
-        subprocess.run(['poetry', 'run', 'jupyter', 'notebook', file_path], check=True)
-    except subprocess.CalledProcessError as e:
+        run(['poetry', 'run', 'jupyter', 'notebook', notebook_file_path], check=True)
+    except CalledProcessError as e:
         print(f"Error: {e}")
 
 # Specify the path to your Jupyter Notebook file
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from data_extraction.run_extraction import run_extraction
 from data_analysis.run_analysis import run_analysis
-import argparse
+
+from argparse import ArgumentParser
+
+from os import listdir
 
 def default_message():
     
@@ -12,16 +15,20 @@ if __name__ == "__main__":
     with open(description_path, 'r') as file:
         program_description = file.read().strip()
     
-    parser = argparse.ArgumentParser(description=program_description)
+    notebook_directory = './data_analysis/notebooks'
+    available_notebooks = [file for file in listdir(notebook_directory) if file.endswith('.ipynb')]
+    
+    parser = ArgumentParser(description=program_description)
 
     # Define command-line options
-    parser.add_argument('-a', action='store_true', help='data analysis')
-    parser.add_argument('-e', action='store_true', help='data extraction')
+    parser.add_argument('-a', '--notebook', help=f'Specify the Jupyter Notebook file containing data analysis. '
+                                                 f'Possible values: {", ".join(available_notebooks)}')
+    parser.add_argument('-e', action='store_true', help='extracting features from photos of particles and writes the results to CSV files.')
 
     # Parse command-line arguments
     args = parser.parse_args()
 
     # Check which option was provided and execute the corresponding function
-    if args.a: run_analysis()
+    if args.notebook: run_analysis(args.notebook)
     elif args.e: run_extraction()
     else: default_message()
